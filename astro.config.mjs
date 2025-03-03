b09eb6dcc93f286a013252b49e693c1aa3eeb5bc
Supawish Kaewjing <supawish.kj@gmail.com>
2025-03-03 22:50:30 +0700

    fix: update base path in configuration for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://well-foundation.github.io",
-  base: "/well-foundation/",
+  base: "/",
   vite: {
     plugins: [tailwindcss()],
   },

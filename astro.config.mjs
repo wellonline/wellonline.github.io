86d9b6a1bfd38d8f111d631bb4cf155e3c245182
Supawish Kaewjing <supawish.kj@gmail.com>
2025-03-03 22:18:25 +0700

    feat: add homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://well-foundation.github.io",
+  vite: {
+    plugins: [tailwindcss()],
+
+  },
 });
